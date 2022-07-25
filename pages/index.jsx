5c004f9be8533fb231ddd13ce68b77615dbb739b
eejay89 <juancarlos.deborja@gmail.com>
2022-07-25 22:45:23 +0800

    ogimage

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,11 +9,12 @@ export default function Home() {
         <title>
           Hell Let Loose SEA - GamersGarage
         </title>
-        <meta name="robots" content="noindex"/>
+        <meta name="robots" content="all"/>
         <meta
           name="description"
           content="Your very first Hell Let Loose South-East Asian English speaking server!"
         />
+        <meta property="og:image" content="https://gamersgarage.tv/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero.037434f3.jpg&w=1920&q=75"/>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
       </Head>
       <Template />
